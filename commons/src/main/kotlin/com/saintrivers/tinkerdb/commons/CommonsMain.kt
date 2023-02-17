4c9375fdb6fcd26f09a2e01b2864265520a5a96f
dayan <eam.dayan@gmail.com>
2023-02-17 08:58:44 +0700

    deployment: add docker scripts

---
+++ commons/src/main/kotlin/com/saintrivers/tinkerdb/commons/CommonsMain.kt
@@ -0,0 +1,10 @@
+package com.saintrivers.tinkerdb.commons
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CommonsMain
+
+fun main() {
+    println("hello world!")
+}
---
+++ docker-utils/src/main/kotlin/com.saintrivers.tinkerdb/docker/DockerUtilsMain.kt
@@ -0,0 +1,10 @@
+package com.saintrivers.tinkerdb.docker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DockerUtilsMain
+
+fun main() {
+    println("hello world!")
+}
